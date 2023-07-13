58124b1f9bc21a80ebc062a4f580b8a0b2f5e7d2
Shraddha J R <71688268+shraddha2000-jr@users.noreply.github.com>
2023-07-13 13:06:50 +0530

    Update streamlit_app.py
    added index

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,4 +9,6 @@ streamlit.text('🥑🍞 avacado toast')
 streamlit.title('Build Your Own Fruit Smoothie')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.multiselect("pick some fruits:",list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)

8c41a2a1272cf0a9d4e66f32c4e4b442aea28a6d
Shraddha J R <71688268+shraddha2000-jr@users.noreply.github.com>
2023-07-13 12:46:33 +0530

    Update streamlit_app.py
    added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents new healthi diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('grape')
+streamlit.text('pasta')
+streamit.text('hard-boilde free-rang egg')
